c8cf31079036c9912ce90bd076848cc48f569bdf
Evgeni <qazplm2125a@gmail.com>
2025-12-15 23:16:23 +0500

    feat(chat): add leave chat functionality and fix rendering logic

--- src/features/chat/chatArea/ChatEmptyArea.tsx
+++ src/features/chat/chatArea/ChatEmptyArea.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+import ModalChatInvite from '../chatInvite/ModalChatInvite';
+
 const ChatEmptyArea = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className="empty-chat">
       <img src="./letter.svg" />
       <div className="empty-chat-text">Выберите чат или создайте новый</div>
-      <button className="create-chat-btn">Создать чат</button>
+      <button className="create-chat-btn" onClick={() => setOpenModal(true)}>
+        Создать чат
+      </button>
+      {openModal && <ModalChatInvite onClose={() => setOpenModal(false)} />}
     </div>
   );
 };
